c214994a7754c1c12121a19dc022d154208c1edc
Max <maksims.skridlevskis@guesty.com>
2024-12-15 13:57:52 +0100

    Move GildedRose logic to a separate class and add initial test

---
+++ gilded-rose/tests/GildedRoseTests.cs
@@ -0,0 +1,19 @@
+﻿using GildedRose.Console;
+
+namespace tests;
+
+public class GildedRoseTests
+{
+    [Fact]
+    public void NormalItem_DecreasesQualityBy1_BeforeSellByDate()
+    {
+        var item = new Item { Name = "Normal Item", SellIn = 10, Quality = 20 };
+        var gildedRose = new GildedRose.Console.GildedRose(new List<Item> { item });
+        
+        gildedRose.UpdateQuality();
+        
+        Assert.Equal(19, item.Quality);
+        Assert.Equal(9, item.SellIn);
+    }
+
+}
